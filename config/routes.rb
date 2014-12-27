ceefc357b5a37077e8493c9ac1f8fe8612013f90
atallahhezbor <atallahhezbor@gmail.com>
2014-12-26 23:19:20 -0500

    Add user log in form and flash tests for invalid logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
 
-  root 'static_pages#home'
+
 
   resources :users
 
